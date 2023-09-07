a45e8c8f4acfe99714eb9be00d860f33446c2852
Ysnaldo Lopez <ysnaldo.lopez@masiv.com>
2023-09-06 20:50:41 -0500

    feature added init cdk methods

---
+++ BlockMaster.CDKApplication/GlobalSuppressions.cs
@@ -0,0 +1,5 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+[assembly:
+    SuppressMessage("Potential Code Quality Issues", "RECS0026:Possible unassigned object created by 'new'",
+        Justification = "Constructs add themselves to the scope in which they are created")]
--- BlockMaster.CDKApplication/Program.cs
+++ BlockMaster.CDKApplication/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System.Diagnostics.CodeAnalysis;
+using Amazon.CDK;
+using BlockMaster.CDKApplication.Props;
+using BlockMaster.CDKApplication.Stacks;
 
 namespace BlockMaster.CDKApplication;
 
@@ -9,6 +12,21 @@ public static class Program
 
     public static void Main(string[] args)
     {
+        var app = new App();
+        var blockMasterStackProps = new BlockMasterStackProps()
+        {
+            ParameterStorePath = app.Node.TryGetContext("ParameterStorePath") as string,
+            MoviesTableName = app.Node.TryGetContext("MoviesTableName") as string
+        };
+        _ = new BlockMasterStack(app, "BlockMasterCdkApplicationStack", new StackProps
+        {
+            Env = new Amazon.CDK.Environment
+            {
+                Account = "", //Here is the value of the account
+                Region = "us-east-1",
+            }
+        }, blockMasterStackProps);
+        app.Synth();
     }
 
     #endregion
