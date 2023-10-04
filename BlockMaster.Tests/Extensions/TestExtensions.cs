fe82739afd2b9ff9b0af9f115eb44d5fc1f70ff7
Ysnaldo Lopez <ysnaldo.lopez@masiv.com>
2023-10-03 20:31:48 -0500

    feature added find movie steps

---
+++ BlockMaster.Tests/Extensions/TestExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using BlockMaster.Domain.Entities;
+using Newtonsoft.Json;
+
+namespace BlockMaster.Tests.Extensions;
+
+public class TestExtensions
+{
+    protected static async Task<Movie> GetMovieFromStreamReader(string path)
+    {
+        using var streamReader = new StreamReader(path);
+        var stringResult = await streamReader.ReadToEndAsync();
+        var movie = JsonConvert.DeserializeObject<Movie>(stringResult);
+
+        return movie;
+    }
+}
